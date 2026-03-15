e6ee08572adea47b68df0386f25e826067fd3a6a
Nadir Ahmed <102181168+CodeKing95@users.noreply.github.com>
2026-03-15 12:25:23 +0000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from './App.tsx'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root')!).render( 
   <StrictMode>
     <App />
   </StrictMode>,
